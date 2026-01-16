8b3af1bf6f3e10469800305a8db4cf0cb13588ca
NAD3R <naderdeghaili@gmail.com>
2026-01-16 13:24:13 +0100

    completed GameCollection methods without Streams, next with Streams

--- src/main/java/naderdeghaili/entities/GameCollection.java
+++ src/main/java/naderdeghaili/entities/GameCollection.java
@@ -5,29 +5,42 @@ import java.util.List;
 import java.util.Map;
 
 public class GameCollection {
+
     Map<Integer, Game> games = new HashMap<>();
 
     public void addGame(Game game) {
+      int id = game.getID();
 
+      if (games.containsKey(id))
+      {throw new IllegalArgumentException("il gioco è già presente nella collezione");}
+      else{ games.put(id, game);}
     }
 
     public Game getById(int id) {
-        return
+        if (games.containsKey(id)){
+        return games.get(id);}
+        else {throw new IllegalArgumentException("il gioco non è presente nella collezione");}
     }
 
     public List<Game> getGamesWithPriceLowerThan(double price) {
         return
     }
 
-    public List<Game> getGamesPerPlayers(int players) {
+    public List<TableGame> getGamesPerPlayers(int players) {
         return
     }
 
     public void removeById(int id) {
+        if (games.containsKey(id))
+        {games.remove(id);}
+        else {throw new IllegalArgumentException("il gioco non è presente nella collezione");}
 
     }
 
     public void updateById(int id, Game newGame) {
+        if (games.containsKey(id))
+        {games.put(id, newGame);}
+        else {throw new IllegalArgumentException("il gioco non è presente nella collezione");}
 
     }
 
