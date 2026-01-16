48cfb3ead2a3be6f95b10fe2c06153fd497bf3f1
NAD3R <naderdeghaili@gmail.com>
2026-01-16 16:03:55 +0100

    completed GameCollection methods with Streams, created gamecolleciton in main and started scanner

--- src/main/java/naderdeghaili/Application.java
+++ src/main/java/naderdeghaili/Application.java
@@ -1,25 +1,55 @@
 package naderdeghaili;
 
-import naderdeghaili.entities.Game;
-import naderdeghaili.entities.Genre;
-import naderdeghaili.entities.TableGame;
-import naderdeghaili.entities.VideoGame;
+import naderdeghaili.entities.*;
+
+import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
 
+        GameCollection gameCollection = new GameCollection();
+
+        //VideoGames
         Game g1 = new VideoGame(1, "Call of Duty", 2025, 80, "MultiPlatform", 30, Genre.FPS);
         Game g2 = new VideoGame(2, "Fortnite", 2017, 0, "MultiPlatform", 100, Genre.TPS);
         Game g3 = new VideoGame(3, "Red Dead Redemption", 2020, 60, "MultiPlatform", 30, Genre.ACTION);
         Game g4 = new VideoGame(4, "Arc Raiders", 2025, 40, "MultiPlatform", 30, Genre.TPS);
-        Game g5 = new VideoGame(5, "Expedition 66", 2024, 40, "MultiPlatform", 30, Genre.ADVENTURE);
 
+        //TableGames
         Game g6 = new TableGame(6, "Monopoly", 1935, 30, 4, 30);
         Game g7 = new TableGame(7, "Risk", 1959, 50, 4, 180);
         Game g8 = new TableGame(8, "Exploding Kittens", 2015, 20, 4, 20);
         Game g9 = new TableGame(9, "Hotel", 1974, 40, 4, 90);
+
+        //Games per funzione update
+        Game g5 = new VideoGame(5, "Expedition 66", 2024, 40, "MultiPlatform", 30, Genre.ADVENTURE);
         Game g10 = new TableGame(10, "Taboo", 1989, 20, 4, 30);
+
+        gameCollection.addGame(g1);
+        gameCollection.addGame(g2);
+        gameCollection.addGame(g3);
+        gameCollection.addGame(g4);
+        gameCollection.addGame(g6);
+        gameCollection.addGame(g7);
+        gameCollection.addGame(g8);
+        gameCollection.addGame(g9);
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("""
+                    MENU
+                    1 - Aggiungi gioco
+                    2 - Ricerca per ID
+                    3 - Ricerca per prezzo massimo
+                    4 - Ricerca giochi da tavolo per numero giocatori
+                    5 - Rimuovi gioco per ID
+                    6 - Aggiorna gioco per ID
+                    7 - Ottieni le statistiche della collezione
+                    """);
+
+        }
 
 
     }
--- src/main/java/naderdeghaili/entities/GameCollection.java
+++ src/main/java/naderdeghaili/entities/GameCollection.java
@@ -1,51 +1,86 @@
 package naderdeghaili.entities;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GameCollection {
 
     Map<Integer, Game> games = new HashMap<>();
 
     public void addGame(Game game) {
-      int id = game.getID();
+        int id = game.getID();
 
-      if (games.containsKey(id))
-      {throw new IllegalArgumentException("il gioco è già presente nella collezione");}
-      else{ games.put(id, game);}
+        if (games.containsKey(id)) {
+            throw new IllegalArgumentException("il gioco è già presente nella collezione");
+        } else {
+            games.put(id, game);
+        }
     }
 
     public Game getById(int id) {
-        if (games.containsKey(id)){
-        return games.get(id);}
-        else {throw new IllegalArgumentException("il gioco non è presente nella collezione");}
+        if (games.containsKey(id)) {
+            return games.get(id);
+        } else {
+            throw new IllegalArgumentException("il gioco non è presente nella collezione");
+        }
     }
 
     public List<Game> getGamesWithPriceLowerThan(double price) {
-        return
+        return games.values()
+                .stream()
+                .filter(game -> game.getPrice() < price)
+                .toList();
     }
 
-    public List<TableGame> getGamesPerPlayers(int players) {
-        return
+    public List<Game> getGamesPerPlayers(int players) {
+        return games.values()
+                .stream()
+                .filter(game -> game instanceof TableGame)
+                .filter(tableGame -> ((TableGame) tableGame).getNumberOfPlayers() >= players).toList();
     }
 
     public void removeById(int id) {
-        if (games.containsKey(id))
-        {games.remove(id);}
-        else {throw new IllegalArgumentException("il gioco non è presente nella collezione");}
+        if (games.containsKey(id)) {
+            games.remove(id);
+        } else {
+            throw new IllegalArgumentException("il gioco non è presente nella collezione");
+        }
 
     }
 
     public void updateById(int id, Game newGame) {
-        if (games.containsKey(id))
-        {games.put(id, newGame);}
-        else {throw new IllegalArgumentException("il gioco non è presente nella collezione");}
+        if (games.containsKey(id)) {
+            games.put(id, newGame);
+        } else {
+            throw new IllegalArgumentException("il gioco non è presente nella collezione");
+        }
 
     }
 
     public void printGameStatistics() {
+
+        long videoGames = games.values()
+                .stream()
+                .filter(game -> game instanceof VideoGame)
+                .count();
+        long tableGames = games.values()
+                .stream()
+                .filter(game -> game instanceof TableGame)
+                .count();
+//          List<Game> highestPrice = games.values().stream().sorted(Comparator.comparing(Game::getPrice)).limit(1).toList().reversed();
+        Optional<Game> highestPrice = games.values()
+                .stream()
+                .max(Comparator.comparing(Game::getPrice));
+        double averagePrice = games.values()
+                .stream()
+                .collect(Collectors.averagingDouble(Game::getPrice));
+        System.out.println("i videogiochi sono: " + videoGames);
+        System.out.println("i giochi da tavolo sono: " + tableGames);
+        System.out.println("il gioco con il prezzo più alto è: " + highestPrice.map(Game::getTitle).orElse("Non ci sono giochi nella collezione"));
+        System.out.println("il prezzo medio dei giochi è: " + averagePrice);
     }
-
-
 }
+
+
+
+
